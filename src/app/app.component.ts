b80b5d6c85b577213afb54e4b1ba7e10be38f875
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2019-05-04 04:20:23 -0300

    fiter data finished just some adjusts left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { State } from './state.module';
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   states: State[];
+  filteredStates:State[];
+  private _searchItem:string;
+  get searchItem():string{
+    return this._searchItem
+  }
+  set searchItem(value: string){
+    this._searchItem = value;
+    this.filteredStates = this.filterName(value)
+  }
+
+  filterName(searchItem: string){
+    return this.states.filter(State => State.nome.toLowerCase().indexOf(searchItem.toLowerCase()) !== -1)
+  }
+
+  
 
 constructor(private dataService: DataService){}
 
 ngOnInit(){
-  return this.dataService.getStates()
-  .subscribe(data => this.states = data)
+   this.dataService.getStates()
+  .subscribe(data => this.states = data);
+this.filteredStates = this.states
+}
+
+triggerViewmore(){
+ document.querySelector('.viewMore').style.display = "block"
+}
+
+fecharViewMore(){
+  document.querySelector('.viewMore').style.display = "none";
 }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { DataService } from './data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
@@ -13,7 +13,8 @@ import { HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
